79a8b22d7a073d9daadc69b414d0f8f403129114
HrytsenkoEN <ivgenius@gmail.com>
2023-03-21 13:10:36 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -32,11 +32,9 @@ function createPromise(position, delay) {
 
   promise
     .then(({ position, delay }) => {
-      // Notiflix.Notify.success
-      console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
-      // Notiflix.Notify.failure
-      console.log(`Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
     });
 }
